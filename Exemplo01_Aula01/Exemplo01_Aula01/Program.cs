db8ba74f2e062bfec8a485719aee11d47c984f3b
Renato <renatocarvalhojunior97@gmail.com>
2021-03-16 18:12:59 -0300

    Conclusão

---
+++ Exemplo01_Aula01/Exemplo01_Aula01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemplo01_Aula01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // construtor "default"
+            Cachorro cao1 = new Cachorro();
+            var cao2 = new Cachorro();
+            Cachorro cao3 = new Cachorro();
+            var cao4 = new Cachorro();
+            var cao5 = new Cachorro();
+
+            Cachorro[] cachorros = { cao1, cao2, cao3, cao4, cao5 };
+
+
+        }
+    }
+}
