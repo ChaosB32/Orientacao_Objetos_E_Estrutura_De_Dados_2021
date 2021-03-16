10ec646df24de41e3cdedfe2dffc5560c585de90
Renato <renatocarvalhojunior97@gmail.com>
2021-03-16 20:50:59 -0300

    Laboratório01 Concluído

---
+++ Laboratorio01/Laboratorio01/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Usuario1 = new Usuario();
+            Usuario1.nome = "Bob Nelson";
+            Usuario1.idade = 37;
+            Usuario1.altura = 1.70f;
+            Usuario1.peso = 78f;
+
+            var Usuario2 = new Usuario();
+            Usuario2.nome = "Testolfo Rocha";
+            Usuario2.idade = 43;
+            Usuario2.altura = 1.65f;
+            Usuario2.peso = 60f;
+
+            var Usuario3 = new Usuario();
+            Usuario3.nome = "Lisa Leite";
+            Usuario3.idade = 22;
+            Usuario3.altura = 1.72f;
+            Usuario3.peso = 92f;
+
+            Usuario1.CalcularImc();
+            Usuario2.CalcularImc();
+            Usuario3.CalcularImc();
+        }
+    }
+}
---
+++ Laboratorio01/Laboratorio01/Usuario.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio01
+{
+    class Usuario
+    {
+        public string nome;
+        public int idade;
+        public float altura;
+        public float peso;
+        public float imc;
+        public float meta;
+
+        public Usuario(float _altura, float _peso)
+        {
+            this.altura = _altura;
+            this.peso = _peso;
+        }
+        public Usuario()
+        {
+
+        }
+        public void CalcularImc()
+        {
+            imc = peso / (altura * altura);
+            meta = 24.9f * (altura * altura);
+
+            if(imc >= 18.5f && imc <= 24.9f)
+            {
+                Console.WriteLine(" Usuário: {0}\n Idade: {1}\n Altura: {2} M\n Peso: {3} Kg\n IMC: {4}\n Situação: {5}\n Meta: {6} Kg ",nome,idade,altura,peso,imc, "Parabéns — você está em seu peso normal! ",meta);
+                Console.ReadLine();
+            }
+            if(imc >= 25.0f && imc <= 29.9f)
+            {
+                Console.WriteLine(" Usuário: {0}\n Idade: {1}\n Altura: {2} M\n Peso: {3} Kg\n IMC: {4}\n Situação: {5}\n Meta: {6} Kg ",nome,idade,altura,peso,imc, "Você está acima de seu peso (sobrepeso). ",meta);
+                Console.ReadLine();
+            }
+            if (imc >= 30f && imc <= 34.9f)
+            {
+                Console.WriteLine(" Usuário: {0}\n Idade: {1}\n Altura: {2} M\n Peso: {3} Kg\n IMC: {4}\n Situação: {5}\n Meta: {6} Kg ", nome, idade, altura, peso, imc, "Obesidade grau I. ",meta);
+                Console.ReadLine();
+            }
+            if (imc >= 35f && imc <= 39.9f)
+            {
+                Console.WriteLine(" Usuário: {0}\n Idade: {1}\n Altura: {2} M\n Peso: {3} Kg\n IMC: {4}\n Situação: {5}\n Meta: {6} Kg ", nome, idade, altura, peso, imc, "Obesidade grau II. ",meta);
+                Console.ReadLine();
+            }
+            if (imc >= 40f)
+            {
+                Console.WriteLine(" Usuário: {0}\n Idade: {1}\n Altura: {2} M\n Peso: {3} Kg\n IMC: {4}\n Situação: {5}\n Meta: {6} Kg ", nome, idade, altura, peso, imc, "Obesidade graus III e IV. ",meta);
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
