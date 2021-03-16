dc5beb8ec17d26626e9f37bf45981fbb631dc335
Renato <renatocarvalhojunior97@gmail.com>
2021-03-16 18:28:28 -0300

    Concluído

---
+++ Exemplo02_Aula01/Exemplo02_Aula01/Cachorro.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemplo02_Aula01
+{
+    class Cachorro
+    {
+        public string nome;
+        public string raca;
+        public int idade;
+        public string genero;
+
+        public void AbanarRabo()
+        {
+            Console.WriteLine("Balançando Rabo!!!");
+            Console.ReadLine();
+        }
+        public void Latir()
+        {
+            Console.WriteLine("Au Au Au!!!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Exemplo02_Aula01/Exemplo02_Aula01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemplo02_Aula01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bidu = new Cachorro();
+            bidu.nome = "Bidu da Silva";
+            bidu.raca = "Pit-bull";
+            bidu.genero = "Macho";
+            bidu.idade = 3;
+
+            bidu.Latir();
+            bidu.AbanarRabo();
+        }
+    }
+}
