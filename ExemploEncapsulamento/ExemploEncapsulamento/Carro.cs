c1919791baad03613f7ae9310103beef5fe144f9
Renato <renatocarvalhojunior97@gmail.com>
2021-03-17 11:30:00 -0300

    Concluido

---
+++ ExemploEncapsulamento/ExemploEncapsulamento/Carro.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploEncapsulamento
+{
+    class Carro
+    {
+        private string modelo;
+        private string cor;
+        private int anoFabricacao;
+        private int velocidadeMaxima;
+        private int velocidadeAtual;
+        private bool seLigado = false;
+
+        public Carro(string modelo, string cor, int anoFabricacao, int velocidadeMaxima)
+        {
+            this.modelo = modelo;
+            this.cor = cor;
+            this.anoFabricacao = anoFabricacao;
+            this.velocidadeMaxima = velocidadeMaxima;
+        }
+
+        public string Modelo { get => modelo;}
+        public string Cor { get => cor;}
+        public int AnoFabricacao { get => anoFabricacao;}
+        public int VelocidadeMaxima { get => velocidadeMaxima;}
+        public int VelocidadeAtual { get => velocidadeAtual;}
+        public bool SeLigado { get => seLigado;}
+
+        public Carro()
+        {
+
+        }
+
+        
+
+        //métodos
+        public void Ligar()
+        {
+            if (seLigado)
+                Console.WriteLine("Carro já está ligado!");
+            else
+            {
+                this.seLigado = true;
+                Console.WriteLine("Carro ligado!");
+                Console.ReadLine();
+            }
+        }
+        public void Desligar()
+        {
+            if (seLigado==false)
+                Console.WriteLine("Carro já está desligado!");
+            else
+            {
+                this.seLigado = false;
+                Console.WriteLine("Carro desligado!");
+            }
+        }
+        public void Acelerar()
+        {
+            if (seLigado)
+            {
+                if (velocidadeAtual <= velocidadeMaxima)
+                {
+                    this.velocidadeAtual = VelocidadeAtual + 10;
+                    Console.WriteLine($"Sua velocidade atual é: {velocidadeAtual}");
+                }
+                else if (velocidadeAtual > velocidadeMaxima)
+                {
+                    Console.WriteLine("Limite de Velocidade");
+                }
+            }
+            else
+            {
+                Console.WriteLine("O carro precisa estar ligado!");
+            }
+        }
+        public void Freiar()
+        {
+            if (velocidadeAtual >= 0)
+            {
+                this.velocidadeAtual = VelocidadeAtual - 10;
+                Console.WriteLine($"Sua velocidade atual é: {velocidadeAtual}");
+            }
+            else if (velocidadeAtual <0)
+            {
+                Console.WriteLine("Carro Parado");
+            }
+        }
+    }
+}
---
+++ ExemploEncapsulamento/ExemploEncapsulamento/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploEncapsulamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro carroBob = new Carro("Fusca","Azul",1978,120);
+            
+            carroBob.Acelerar();
+            Console.ReadLine();
+        }
+    }
+}
