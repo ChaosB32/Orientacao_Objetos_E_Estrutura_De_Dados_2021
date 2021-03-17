1169a5e56d466e984316f206b063685b45fe7673
Renato <renatocarvalhojunior97@gmail.com>
2021-03-17 10:36:38 -0300

    Concluído

---
+++ CalcularImc_Lab01/CalcularImc_Lab01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcularImc_Lab01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Usuario bobNelson = new Usuario("Bob Nelson", 37, 78.0f, 1.70f);
+            Console.WriteLine($"Situacao IMC do {bobNelson.nome}: {bobNelson.InformarSituacaoImc()}");
+            Console.WriteLine($"Seu peso ideal é {bobNelson.InformarMetaPeso()}");
+            Console.ReadLine();
+
+            Usuario testolfo = new Usuario("Testolfo Rocha", 43, 60.0f, 1.65f);
+            Console.WriteLine($"Situacao IMC do {testolfo.nome}: {testolfo.InformarSituacaoImc()}");
+            Console.WriteLine($"Seu peso ideal é {testolfo.InformarMetaPeso()}");
+            Console.ReadLine();
+
+            Usuario lisa = new Usuario("Lisa Leite", 22, 92.0f, 1.72f);
+            Console.WriteLine($"Situacao IMC do {lisa.nome}: {lisa.InformarSituacaoImc()}");
+            Console.WriteLine($"Seu peso ideal é {lisa.InformarMetaPeso()}");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ CalcularImc_Lab01/CalcularImc_Lab01/Usuario.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcularImc_Lab01
+{
+    class Usuario
+    {
+        public string nome;
+        public int idade;
+        public float peso;
+        public float altura;
+
+        public Usuario(string nome, int idade, float peso, float altura)
+        {
+            this.nome = nome;
+            this.idade = idade;
+            this.peso = peso;
+            this.altura = altura;
+        }
+
+        private float CalcularImc()
+        {
+            return this.peso / (this.altura * this.altura);
+        }
+        
+        public string InformarSituacaoImc()
+        {
+            float imc = CalcularImc();
+            string situacaoImc = "Erro de identificação";
+
+            if(imc>=18.5f && imc <= 24.9f)
+                situacaoImc = "Parabéns — você está em seu peso normal!";
+            if (imc >= 25.0f && imc <= 29.9f)
+                situacaoImc = "Você está acima de seu peso (sobrepeso).";
+            if (imc >= 30.0f && imc <= 34.9f)
+                situacaoImc = "Obesidade grau I.";
+            if (imc >= 35.0f && imc <= 39.9f)
+                situacaoImc = "Obesidade grau II.";
+            if (imc >= 40.0f)
+                situacaoImc = "Obesidade graus III e IV.";
+            return situacaoImc;
+        }
+        
+        public float InformarMetaPeso()
+        {
+            float imcIdeal = 24.9f;
+            float pesoIdeal = imcIdeal * (this.altura) * (this.altura);
+            return pesoIdeal;
+        }
+    }
+}
