02f603fe99a8daf679584de2e876bf89b9bfea36
Renato Carvalho <renatocarvalhojunior97@gmail.com>
2021-05-05 12:15:39 -0300

    Concluído

---
+++ ImplementarPilhaPorArray/ImplementarPilhaPorArray/MinhaPilha.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImplementarPilhaPorArray
+{
+    class MinhaPilha
+    {
+        public int Tamanho { get; set; }
+        public int Topo { get; set; }
+        public string[] Dados { get; set; }
+
+        public MinhaPilha(int tamanho)
+        {
+            Tamanho = tamanho;
+            Topo = -1;
+            Dados = new string[tamanho];
+        }
+
+        public void Push(string elemento)
+        {
+            if(Topo < Tamanho-1)
+            {
+                Topo = Topo + 1;
+                Dados[Topo] = elemento;
+                Console.WriteLine("Push: " + elemento);
+            }
+            else
+            {
+                Console.WriteLine("Pilha já está cheia!");
+            }
+        }
+        public void Pop()
+        {
+            if(Topo == -1)
+            {
+                Console.WriteLine("A pilha está vazia!");
+            }
+            else
+            {
+                Console.WriteLine("Pop: " + Dados[Topo]);
+                Topo = Topo - 1;
+            }
+        }
+        public void ImprimirPilha()
+        {
+            if(Topo == -1)
+            {
+                Console.WriteLine("A pilha está vazia!");
+            }
+            else
+            {
+                for (int i = Topo; i >= 0; i--)
+                {
+                    Console.WriteLine(Dados[i]);
+                }
+            }
+        }
+        public string Peek()
+        {
+            return Dados[Topo];
+        }
+    }
+}
---
+++ ImplementarPilhaPorArray/ImplementarPilhaPorArray/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ImplementarPilhaPorArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MinhaPilha primeiraPilha = new MinhaPilha(3);
+            primeiraPilha.Pop();
+            primeiraPilha.Push("Goccom");
+            primeiraPilha.Push("Kazuto");
+            primeiraPilha.Push("Sr.M");
+            primeiraPilha.Pop();
+            primeiraPilha.Push("Bob Nelson");
+            Console.WriteLine("\nElementos na Pilha:");
+            primeiraPilha.ImprimirPilha();
+            primeiraPilha.Peek();
+            Console.ReadLine();
+        }
+    }
+}
