7b5775d727ef9c3c0a2c56303739c148b049fb3a
Renato Carvalho <renatocarvalhojunior97@gmail.com>
2021-05-19 17:50:46 -0300

    completo

---
+++ ListaEspera/ListaEspera/FilaEstatica.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListaEspera
+{
+    class FilaEstatica
+    {
+        public FilaEstatica(int elementos)
+        {
+            Dados = new int[elementos];
+            Inicio = 0;
+            Fim = -1;
+        }
+
+        public int Inicio { get; set; }
+        public int Fim { get; set; }
+        public int[] Dados { get; set; }
+
+        public void Enfileirar(int numero)
+        {
+            if (Fim >= Dados.Length - 1)
+            {
+                Console.WriteLine("A fila está cheia!");
+                return;
+            }
+            else
+            {
+                Fim++;
+                Dados[Fim] = numero;
+                Console.WriteLine($"Elemento {numero} foi inserido!");
+            }
+        }
+
+        public void Imprimir()
+        {
+            if (Fim == -1)
+            {
+                Console.WriteLine("A fila está vazia!");
+                return;
+            }
+            for (int i = Inicio; i <= Fim; i++)
+            {
+                Console.WriteLine(Dados[i]);
+            }
+        }
+    }
+}
---
+++ ListaEspera/ListaEspera/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ListaEspera
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fila com 5 números:");
+            FilaEstatica minhaFila = new FilaEstatica(4);
+            minhaFila.Enfileirar(5);
+            minhaFila.Enfileirar(10);
+            minhaFila.Enfileirar(12);
+            minhaFila.Enfileirar(6);
+            Console.WriteLine("Imprimir os valores da fila:");
+            minhaFila.Imprimir();
+            Console.ReadLine();
+        }
+    }
+}
